a78eccbd1900f1b839e3338532e2cb3aa52fd7a0
Khadijagardezi <khadijagardezi5@gmail.com>
2020-09-14 11:51:41 +0500

    Day 1

---
+++ day-1.js
@@ -0,0 +1,14 @@
+// Day 1 JavaScript Coding Challenge
+// Print numbers from 0 to 9
+
+for(let i=0; i<10; i++) {
+    i;
+}
+
+// Sharing a quotation daily
+/*
+
+Success isn't about being the best. 
+It's about always getting better.
+
+*/
