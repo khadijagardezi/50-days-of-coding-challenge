454b41bf6a230687189797ddf0d81663e6e40035
Khadijagardezi <khadijagardezi5@gmail.com>
2020-09-16 12:52:48 +0500

    Day 3

---
+++ day-3.js
@@ -0,0 +1,14 @@
+// Day 3 JavaScript Coding Challenge
+// Print the multiplication table with 7
+
+for(let i=1; i<=10; i++) {
+  let tab = "7 * " + i + " = " + 7 * i;
+  return tab;
+}
+
+/*
+
+If people never did silly things, nothing intelligent would ever get done.
+
+
+*/
